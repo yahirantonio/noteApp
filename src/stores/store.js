46e8b116cfd770f90341cfa9088648aa5dc9aa16
Yahir Antonio Diaz Coronado <yahir.coronado@villagroup.com>
2024-12-05 14:35:39 -0600

    fix(store): correcion de formato de fecha para consultas

--- src/stores/store.js
+++ src/stores/store.js
@@ -2,8 +2,9 @@ import { readable, writable } from "svelte/store";
 
 export const invisibleBanner = writable(true);
 
-let dia = new Date()
-let date = `${dia.getFullYear().toString()}-${String(dia.getMonth() + 1)}-${dia.getDate().toString()}`
+let actualDate = new Date()
+// let date = `${dia.getFullYear().toString()}-${String(dia.getMonth() + 1)}-${dia.getDate().toString()}`
+let date = actualDate.toISOString().split("T")[0];
 export const today = readable('', (set) => { set(date) })
 
 export const dataStatus = readable([
@@ -36,7 +37,7 @@ export const dataNotes = writable([
       "titulo": "Ejemplo de título",
       "texto": "Este es el contenido de la nota.",
       "etiqueta": "Personal",
-      "fecha": "2024-12-04",
+      "fecha": date,
       "content": { ops: [{ insert: 'Este es el contenido de la nota.\n' }] },
       "estadoID": 0
    },
@@ -45,7 +46,7 @@ export const dataNotes = writable([
       "titulo": "Lista de compras",
       "texto": "Necesito comprar frutas, verduras, pan, leche, huevos y carne. También debo verificar si hay suficiente café en casa y, si no, añadirlo a la lista.",
       "etiqueta": "Personal",
-      "fecha": "2024-12-04",
+      "fecha": date,
       "content": { ops: [{ insert: 'Necesito comprar frutas, verduras, pan, leche, huevos y carne. También debo verificar si hay suficiente café en casa y, si no, añadirlo a la lista.\n' }] },
       "estadoID": 1
    },
@@ -54,7 +55,7 @@ export const dataNotes = writable([
       "titulo": "Planificación de proyecto",
       "texto": "El proyecto requiere coordinar varias tareas: asignación de roles, establecer fechas límite, y preparar reuniones semanales para el seguimiento. Es crucial definir el alcance y los entregables en la primera reunión.",
       "etiqueta": "Trabajo",
-      "fecha": "2024-12-04",
+      "fecha": date,
       "content": { ops: [{ insert: 'El proyecto requiere coordinar varias tareas: asignación de roles, establecer fechas límite, y preparar reuniones semanales para el seguimiento. Es crucial definir el alcance y los entregables en la primera reunión.\n' }] },
       "estadoID": 2
    },
@@ -63,7 +64,7 @@ export const dataNotes = writable([
       "titulo": "Ideas para el blog",
       "texto": "Investigar sobre temas actuales en tecnología como inteligencia artificial, ciberseguridad, y avances en desarrollo web. Preparar un borrador inicial para cada tema y buscar imágenes relevantes para ilustrar los artículos.",
       "etiqueta": "Creativo",
-      "fecha": "2024-12-04",
+      "fecha": date,
       "content": { ops: [{ insert: 'Investigar sobre temas actuales en tecnología como inteligencia artificial, ciberseguridad, y avances en desarrollo web. Preparar un borrador inicial para cada tema y buscar imágenes relevantes para ilustrar los artículos.\n' }] },
       "estadoID": 2
    },
@@ -72,7 +73,7 @@ export const dataNotes = writable([
       "titulo": "Rutina de ejercicio",
       "texto": "Crear una rutina de ejercicios semanal: lunes, miércoles y viernes para entrenamiento de fuerza; martes y jueves para cardio; y sábado para yoga. También debo recordar calentar antes de cada sesión y estirar al final.",
       "etiqueta": "Salud",
-      "fecha": "2024-12-04",
+      "fecha": date,
       "content": { ops: [{ insert: 'Crear una rutina de ejercicios semanal: lunes, miércoles y viernes para entrenamiento de fuerza; martes y jueves para cardio; y sábado para yoga. También debo recordar calentar antes de cada sesión y estirar al final.\n' }] },
       "estadoID": 1
    },
@@ -81,7 +82,7 @@ export const dataNotes = writable([
       "titulo": "Resumen de reunión",
       "texto": "En la reunión discutimos los puntos clave del proyecto: definir prioridades, delegar tareas específicas y acordar un cronograma inicial. La próxima reunión será el viernes para evaluar el progreso y ajustar el plan según sea necesario.",
       "etiqueta": "Trabajo",
-      "fecha": "2024-12-04",
+      "fecha": date,
       "content": { ops: [{ insert: 'En la reunión discutimos los puntos clave del proyecto: definir prioridades, delegar tareas específicas y acordar un cronograma inicial. La próxima reunión será el viernes para evaluar el progreso y ajustar el plan según sea necesario.\n' }] },
       "estadoID": 0
    }
